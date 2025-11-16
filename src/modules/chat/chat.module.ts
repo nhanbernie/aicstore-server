1ab3bd2096729f0338ec3ee08f552e6a7a86e9af
tnhanbernie <ttnhanbernie@gmail.com>
2025-11-16 10:32:15 +0700

    fix: chat vendor

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { Conversation, Message } from './entities';
+import { Vendor } from '@modules/vendors/entity/vendor.schema';
 import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Conversation, Message]),
+    TypeOrmModule.forFeature([Conversation, Message, Vendor]),
     AuthModule,
   ],
   controllers: [ChatController],
